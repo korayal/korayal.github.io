b645d8d6a381cc1a66912e295281a1686a1496ae
Koray Al <koray.al@soostone.com.tr>
2016-12-28 13:36:10 +0300

    Added deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ git stash
 git checkout develop
 
 # Build new files
-stack exec myblog clean
-stack exec myblog build
+stack exec site clean
+stack exec site build
 
 # Get previous files
 git fetch --all
